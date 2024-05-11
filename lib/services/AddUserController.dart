7e1df177d9e7d452891f8f8e5b0633f6e84f505d
xasham <xashams@gmail.com>
2024-05-11 19:46:04 +0700

    adfa

--- lib/services/AddUserController.dart
+++ lib/services/AddUserController.dart
@@ -1,23 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:provider/provider.dart';
 
 class AddUserController extends GetxController {
-  late TextEditingController namac = TextEditingController();
-  late TextEditingController alamatc = TextEditingController();
-  late TextEditingController telepon = TextEditingController();
-  late TextEditingController tanggalc = TextEditingController();
-  late TextEditingController namaorangtuac = TextEditingController();
-  late TextEditingController asalsekolahc = TextEditingController();
-  late TextEditingController alamatsekolahc = TextEditingController();
-
+  late TextEditingController namac;
+  late TextEditingController alamatc;
+  late TextEditingController telepon;
+  late TextEditingController tanggalc;
+  late TextEditingController namaorangtuac;
+  late TextEditingController asalsekolahc;
+  late TextEditingController alamatsekolahc;
   FirebaseFirestore db = FirebaseFirestore.instance;
-   bool is_notif = false;
   void add(String nama, String alamat, String telpon, String tanggal,
       String namaorangtua, String asalsekolah, String alamatsekolah) async {
     CollectionReference users = db.collection("user");
-  
     try {
       await users.add({
         "Nama": nama,
@@ -28,36 +24,29 @@ class AddUserController extends GetxController {
         "Asalsekolah": asalsekolah,
         "Alamatsekolah": alamatsekolah
       });
-      clear();
-    // is_notif = true;
-    // notifyListeners();
-
-    // Get.defaultDialog(
-    //   title: "Halo",
-    //   middleText: "aalal",
-    //   onConfirm: (){
-    //     Get.back();
-    //   }
-
-    // );
-     
+      
+      
     } catch (e) {
       print(e);
-    }
-  }
   
-
-  clear(){
-     namac.clear();
-      alamatc.clear();
-      telepon.clear();
-      tanggalc.clear();
-      namaorangtuac.clear();
-      asalsekolahc.clear();
-      alamatsekolahc.clear();
+    }
+  
   }
+
   @override
-  onClose() {
+  void onInit() {
+    namac = TextEditingController();
+    alamatc = TextEditingController();
+    telepon = TextEditingController();
+    tanggalc = TextEditingController();
+    namaorangtuac = TextEditingController();
+    asalsekolahc = TextEditingController();
+    alamatsekolahc = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
     namac.dispose();
     alamatc.dispose();
     telepon.dispose();
@@ -66,13 +55,5 @@ class AddUserController extends GetxController {
     asalsekolahc.dispose();
     alamatsekolahc.dispose();
     super.onClose();
-  }
-   void defaultDailog() {
-    Future.delayed(const Duration(seconds: 1), () {
-      print(Get.isDialogOpen);
-    });
-
-    Get.defaultDialog(
-        title: 'GetX alert', middleText: 'Hello, here is dialog in getx');
   }
 }
